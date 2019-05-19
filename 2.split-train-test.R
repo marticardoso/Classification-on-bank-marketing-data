3636426f38a99518ef1a99116d86178541c0096a
marticardoso <marti.cardoso@est.fib.upc.edu>
2019-05-19 20:06:04 +0200

    Missing files

--- 2.split-train-test.R
+++ 2.split-train-test.R
@@ -15,9 +15,11 @@ setwd(".")
 
 #Load preprocessed data
 load("bank-processed.Rdata")
+load("bank-processed-cat.Rdata")
+
+# First, we split the dataset, and finally we apply the same split to dataset.cat
 
 set.seed (104)
-
 # Shuffle the data
 shuffle <- function(data){
   data[sample(nrow(data)),]
@@ -25,18 +27,22 @@ shuffle <- function(data){
 dataset <- shuffle(dataset)
 
 # Split data into train and test: stratified split (2/3 train, 1/3 test)
-dataset.y.True <- dataset[dataset$y == TRUE,]
-dataset.y.False <- dataset[dataset$y == FALSE,]
+dataset.y.Yes <- dataset[dataset$y == 'yes',]
+dataset.y.No <- dataset[dataset$y == 'no',]
 
-dataset.y.True.train.idx <- 1:floor(2/3*nrow(dataset.y.True)) # Take the first rows, no need to sample because the data has been shuffled before
-dataset.y.False.train.idx <- 1:floor(2/3*nrow(dataset.y.False))
+dataset.y.Yes.train.idx <- 1:floor(2/3*nrow(dataset.y.Yes)) # Take the first rows, no need to sample because the data has been shuffled before
+dataset.y.No.train.idx <- 1:floor(2/3*nrow(dataset.y.No))
 
 #Join both cases
-dataset.train <- rbind(dataset.y.True[dataset.y.True.train.idx,],
-                       dataset.y.False[dataset.y.False.train.idx,])
-dataset.test <- rbind(dataset.y.True[-dataset.y.True.train.idx,],
-                      dataset.y.False[-dataset.y.False.train.idx,])
+dataset.train <- rbind(dataset.y.Yes[dataset.y.Yes.train.idx,],
+                       dataset.y.No[dataset.y.No.train.idx,])
+dataset.test <- rbind(dataset.y.Yes[-dataset.y.Yes.train.idx,],
+                      dataset.y.No[-dataset.y.No.train.idx,])
 
 #Save data
-save(dataset.train, file = "bank-processed-train.Rdata")
-save(dataset.test, file = "bank-processed-test.Rdata")
+save(dataset.train, dataset.test, file = "bank-processed-train-test.Rdata")
+
+# Apply same split (exact) to dataset.cat and save it
+dataset.cat.train <- dataset.cat[row.names(dataset.train),]
+dataset.cat.test <- dataset.cat[row.names(dataset.test),]
+save(dataset.cat.train, dataset.cat.test, file = "bank-processed-cat-train-test.Rdata")
